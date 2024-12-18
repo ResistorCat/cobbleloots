59e1a81b65ff3f316078eb60f6be1b955035a4ee
Francisco Arenas <francis.arenasv@uc.cl>
2024-12-18 17:46:29 -0300

    Add logger

--- common/src/main/java/dev/ripio/cobbleloots/Cobbleloots.java
+++ common/src/main/java/dev/ripio/cobbleloots/Cobbleloots.java
@@ -1,7 +1,11 @@
 package dev.ripio.cobbleloots;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class Cobbleloots {
     public static final String MOD_ID = "cobbleloots";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
 
