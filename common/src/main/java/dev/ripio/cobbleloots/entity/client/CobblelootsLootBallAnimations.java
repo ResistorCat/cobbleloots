9c85298f15be203b64c16e51cd3506658de02058
Francisco Arenas <francis.arenasv@uc.cl>
2025-02-22 17:54:50 -0300

    Added Loot Balls animations and minor fixes to the model

--- common/src/main/java/dev/ripio/cobbleloots/entity/client/CobblelootsLootBallAnimations.java
+++ common/src/main/java/dev/ripio/cobbleloots/entity/client/CobblelootsLootBallAnimations.java
@@ -6,11 +6,28 @@ import net.minecraft.client.animation.Keyframe;
 import net.minecraft.client.animation.KeyframeAnimations;
 
 public class CobblelootsLootBallAnimations {
-  public static final AnimationDefinition ANIM_LOOT_BALL_OPENING = AnimationDefinition.Builder.withLength(1f)
-      .addAnimation("lid",
+  public static final AnimationDefinition ANIM_LOOT_BALL_SHAKE = AnimationDefinition.Builder.withLength(2.5f)
+      .addAnimation("ball",
           new AnimationChannel(AnimationChannel.Targets.ROTATION,
               new Keyframe(0f, KeyframeAnimations.degreeVec(0f, 0f, 0f),
                   AnimationChannel.Interpolations.LINEAR),
-              new Keyframe(1f, KeyframeAnimations.degreeVec(-75f, 0f, 0f),
-                  AnimationChannel.Interpolations.LINEAR))).build();
+              new Keyframe(0.125f, KeyframeAnimations.degreeVec(-10f, 0f, 5f),
+                  AnimationChannel.Interpolations.CATMULLROM),
+              new Keyframe(0.25f, KeyframeAnimations.degreeVec(-2.17f, -2.58f, -9.78f),
+                  AnimationChannel.Interpolations.CATMULLROM),
+              new Keyframe(0.375f, KeyframeAnimations.degreeVec(-2.6f, -7.16f, 0.45f),
+                  AnimationChannel.Interpolations.CATMULLROM),
+              new Keyframe(0.5f, KeyframeAnimations.degreeVec(0f, 0f, 0f),
+                  AnimationChannel.Interpolations.CATMULLROM))).build();
+  public static final AnimationDefinition ANIM_LOOT_BALL_LID_OPEN = AnimationDefinition.Builder.withLength(2.5f)
+      .addAnimation("lid",
+          new AnimationChannel(AnimationChannel.Targets.ROTATION,
+              new Keyframe(0.70833f, KeyframeAnimations.degreeVec(0f, 0f, 0f),
+                  AnimationChannel.Interpolations.LINEAR),
+              new Keyframe(0.91667f, KeyframeAnimations.degreeVec(-75f, 0f, 0f),
+                  AnimationChannel.Interpolations.CATMULLROM),
+              new Keyframe(2.25f, KeyframeAnimations.degreeVec(-75f, 0f, 0f),
+                  AnimationChannel.Interpolations.CATMULLROM),
+              new Keyframe(2.5f, KeyframeAnimations.degreeVec(0f, 0f, 0f),
+                  AnimationChannel.Interpolations.CATMULLROM))).build();
 }
--- common/src/main/java/dev/ripio/cobbleloots/entity/client/CobblelootsLootBallModel.java
+++ common/src/main/java/dev/ripio/cobbleloots/entity/client/CobblelootsLootBallModel.java
@@ -16,22 +16,26 @@ import static dev.ripio.cobbleloots.util.CobblelootsUtils.cobblelootsResource;
 public class CobblelootsLootBallModel<T extends CobblelootsLootBall> extends HierarchicalModel<T> {
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(cobblelootsResource("loot_ball"), "main");
 	private final ModelPart main;
+	private final ModelPart ball;
 	private final ModelPart lid;
 
 	public CobblelootsLootBallModel(ModelPart root) {
 		this.main = root.getChild("main");
-		this.lid = this.main.getChild("lid");
+		this.ball = this.main.getChild("ball");
+		this.lid = this.ball.getChild("lid");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition main = partdefinition.addOrReplaceChild("main", CubeListBuilder.create().texOffs(32, 20).addBox(-4.0F, -4.0F, -4.0F, 8.0F, 4.0F, 8.0F, new CubeDeformation(0.25F))
-				.texOffs(32, 0).addBox(-4.0F, -4.0F, -4.0F, 8.0F, 4.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition main = partdefinition.addOrReplaceChild("main", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-		PartDefinition lid = main.addOrReplaceChild("lid", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -4.0F, -8.0F, 8.0F, 4.0F, 8.0F, new CubeDeformation(0.0F))
-				.texOffs(0, 0).addBox(-1.0F, -1.0F, -8.25F, 2.0F, 2.0F, 0.0F, new CubeDeformation(0.0F))
+		PartDefinition ball = main.addOrReplaceChild("ball", CubeListBuilder.create().texOffs(32, 20).addBox(-4.0F, -4.0F, -4.0F, 8.0F, 4.0F, 8.0F, new CubeDeformation(0.25F))
+				.texOffs(32, 0).addBox(-4.0F, -4.0F, -4.0F, 8.0F, 4.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+		PartDefinition lid = ball.addOrReplaceChild("lid", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -4.0F, -8.0F, 8.0F, 4.0F, 8.0F, new CubeDeformation(0.0F))
+				.texOffs(-1, -1).addBox(-1.0F, -1.0F, -8.25F, 2.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
 				.texOffs(0, 20).addBox(-4.0F, -4.0F, -8.0F, 8.0F, 4.0F, 8.0F, new CubeDeformation(0.25F)), PartPose.offset(0.0F, -4.0F, 4.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 32);
@@ -40,7 +44,8 @@ public class CobblelootsLootBallModel<T extends CobblelootsLootBall> extends Hie
 	@Override
 	public void setupAnim(CobblelootsLootBall entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 		this.root().getAllParts().forEach(ModelPart::resetPose);
-		this.animate(entity.openingAnimationState, CobblelootsLootBallAnimations.ANIM_LOOT_BALL_OPENING, ageInTicks, 1f);
+		this.animate(entity.openingAnimationState, CobblelootsLootBallAnimations.ANIM_LOOT_BALL_SHAKE, ageInTicks, 1f);
+		this.animate(entity.openingAnimationState, CobblelootsLootBallAnimations.ANIM_LOOT_BALL_LID_OPEN, ageInTicks, 1f);
 	}
 
 	@Override
@@ -52,4 +57,6 @@ public class CobblelootsLootBallModel<T extends CobblelootsLootBall> extends Hie
 	public @NotNull ModelPart root() {
 		return main;
 	}
+
+
 }
