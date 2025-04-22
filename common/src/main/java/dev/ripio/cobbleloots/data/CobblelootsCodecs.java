16253524879617cd5829e9364e667024645710a6
Francisco Arenas <francis.arenasv@uc.cl>
2025-04-21 22:14:25 -0400

    Refactor biome tag default values in codecs

--- common/src/main/java/dev/ripio/cobbleloots/data/CobblelootsCodecs.java
+++ common/src/main/java/dev/ripio/cobbleloots/data/CobblelootsCodecs.java
@@ -11,14 +11,11 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.ComponentSerialization;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.level.biome.Biome;
 
 import java.util.List;
 
 public class CobblelootsCodecs {
-  private static final Integer DEFAULT_WEIGHT = 1;
-  private static final ResourceLocation EMPTY = CobblelootsDefinitions.EMPTY_LOCATION;
-  public static final TagKey<Biome> EMPTY_BIOME_TAG = TagKey.create(Registries.BIOME, EMPTY);
+  private static final int DEFAULT_WEIGHT = 1;
   private static final int MAX_ABSOLUTE_HEIGHT = 10_000_000;
   private static final CobblelootsLootBallHeight DEFAULT_HEIGHT = new CobblelootsLootBallHeight(-MAX_ABSOLUTE_HEIGHT, MAX_ABSOLUTE_HEIGHT);
 
@@ -32,7 +29,7 @@ public class CobblelootsCodecs {
   public static final Codec<CobblelootsLootBallSource> LOOT_BALL_SOURCE_CODEC = RecordCodecBuilder.create(
       instance -> instance.group(
           Codec.STRING.fieldOf("type").forGetter(CobblelootsLootBallSource::getType),
-          TagKey.codec(Registries.BIOME).optionalFieldOf("biome", EMPTY_BIOME_TAG).forGetter(CobblelootsLootBallSource::getBiome),
+          TagKey.codec(Registries.BIOME).optionalFieldOf("biome", CobblelootsDefinitions.EMPTY_BIOME_TAG).forGetter(CobblelootsLootBallSource::getBiome),
           LOOT_BALL_HEIGHT_CODEC.optionalFieldOf("height", DEFAULT_HEIGHT).forGetter(CobblelootsLootBallSource::getHeight),
           Codec.INT.optionalFieldOf("weight", DEFAULT_WEIGHT).forGetter(CobblelootsLootBallSource::getWeight)
       ).apply(instance, CobblelootsLootBallSource::new)
@@ -45,8 +42,8 @@ public class CobblelootsCodecs {
               ComponentSerialization.flatCodec(30).optionalFieldOf("name", Component.empty()).forGetter(CobblelootsLootBallData::getName),
               Codec.BOOL.optionalFieldOf("announce", false).forGetter(CobblelootsLootBallData::getAnnounce),
               Codec.INT.optionalFieldOf("weight", DEFAULT_WEIGHT).forGetter(CobblelootsLootBallData::getWeight),
-              ResourceLocation.CODEC.optionalFieldOf("loot_table", EMPTY).forGetter(CobblelootsLootBallData::getLootTable),
-              ResourceLocation.CODEC.optionalFieldOf("texture", EMPTY).forGetter(CobblelootsLootBallData::getTexture),
+              ResourceLocation.CODEC.optionalFieldOf("loot_table", CobblelootsDefinitions.EMPTY_LOCATION).forGetter(CobblelootsLootBallData::getLootTable),
+              ResourceLocation.CODEC.optionalFieldOf("texture", CobblelootsDefinitions.EMPTY_LOCATION).forGetter(CobblelootsLootBallData::getTexture),
               LOOT_BALL_SOURCE_CODEC.listOf().optionalFieldOf("sources", List.of()).forGetter(CobblelootsLootBallData::getSources),
               selfCodec.listOf().optionalFieldOf("variants", List.of()).forGetter(CobblelootsLootBallData::getVariants)
           ).apply(instance, CobblelootsLootBallData::new)
